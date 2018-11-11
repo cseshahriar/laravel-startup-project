d3c4e68eec1c032110a87b4409cb7949c9da2368
shahriarcse <cse.shahriar.hosen@gmail.com>
2018-11-11 13:40:23 +0600

    Customize User Table

--- resources/assets/js/components/Dashboard.vue
+++ resources/assets/js/components/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container mt-2">
+    <div class="container mt-3">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card card-default">
