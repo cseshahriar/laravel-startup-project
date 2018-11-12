ee0e257e58bc7ee1a4c6a632d4b7d03cc5c744b5
shahriarcse <cse.shahriar.hosen@gmail.com>
2018-11-12 23:17:07 +0600

    API Resource Controller in Laravel with Vue

--- resources/assets/js/components/Users.vue
+++ resources/assets/js/components/Users.vue
@@ -46,14 +46,16 @@
         <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
+              
               <div class="modal-header">
                 <h5 class="modal-title" id="addNewLabel">Add New</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
+
+              <form @submit.prevent="createUser"> <!-- prevent for not refresh the page -->
               <div class="modal-body">
-    
                   <div class="form-group">  
                     <input v-model="form.name" type="text" name="name" placeholder="Name" 
                       class="form-control" :class="{ 'is-invalid': form.errors.has('name') }">
@@ -66,13 +68,11 @@
                     <has-error :form="form" field="email"></has-error>
                   </div>
   
-
                   <div class="form-group">  
                       <textarea name="bio" v-model="form.bio" id="bio" placeholder="Short bio for user (Optional)" class="form-control" :class="{ 'is-invalid' : form.errors.has('bio') }" cols="30" rows="5"></textarea>
                       </has-error :form="form" field="bio"></has-error>
                   </div>
   
-
                   <div class="form-group">  
                       <select name="type" id="type" v-model="form.type" class="form-control" :class="{ 'is-invalid' : form.errors.has('type') }">
                         <option value="">Select User Role</option>
@@ -82,20 +82,18 @@
                       </select>
                       </has-error :form="form" field="type"></has-error> 
                   </div>
-  
 
-                  <div class="form-group">  
+                  <div class="form-group">   
                      <input type="password" v-model="form.password" name="password" id="password" class="form-control" :class="{ 'is-invalid' : form.errors.has('password') }">
                       </has-error :form="form" field="password"></has-error> 
                   </div>
-
-
-
               </div>
+
               <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                  <button type="button" class="btn btn-primary">Create</button>
               </div>
+            </form>
             </div>
           </div>
         </div>
@@ -119,6 +117,12 @@
           })
         }
       },
+      methods: {
+        createUser() {
+           this.form.post('api/user'); 
+        }
+      }
+      ,
       mounted() {
           console.log('Component mounted.')
       }
