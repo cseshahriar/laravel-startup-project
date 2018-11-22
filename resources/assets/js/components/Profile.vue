93c028bef2dd07027f03dfdbac41335f8b3350d6
shahriarcse <cse.shahriar.hosen@gmail.com>
2018-11-22 21:15:02 +0600

    Populate Login User Profile with Laravle and Vuejs

--- resources/assets/js/components/Profile.vue
+++ resources/assets/js/components/Profile.vue
@@ -185,21 +185,14 @@
                         <label for="inputName" class="col-sm-2 control-label">Name</label>
 
                         <div class="col-sm-10">
-                          <input type="email" class="form-control" id="inputName" placeholder="Name">
+                          <input type="email" name="name" v-model="form.name" class="form-control" id="inputName" placeholder="Name">
                         </div>
                       </div>
                       <div class="form-group">
                         <label for="inputEmail" class="col-sm-2 control-label">Email</label>
 
                         <div class="col-sm-10">
-                          <input type="email" class="form-control" id="inputEmail" placeholder="Email">
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <label for="inputName2" class="col-sm-2 control-label">Name</label>
-
-                        <div class="col-sm-10">
-                          <input type="text" class="form-control" id="inputName2" placeholder="Name">
+                          <input type="email" v-model="form.email" class="form-control" id="inputEmail" name="email" placeholder="Email">
                         </div>
                       </div>
                       <div class="form-group">
@@ -248,8 +241,24 @@
 
 <script>
     export default {
+        data() {
+          return {
+            form : new Form({  
+              id: '', 
+              name: '',
+              email: '',
+              password: '',
+              type: '',
+              bio: '',
+              photo: ''
+            })
+          }
+        },
         mounted() {
             console.log('Component mounted.') 
+        },
+        created() {
+          axios.get('api/profile').then( ({data}) => (this.form.fill(data)) );
         }
     }
 </script>
